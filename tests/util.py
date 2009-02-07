f163547011d5b4c9bf3dc874c46202d40886bd20
exogen <devnull@localhost>
2009-02-07 19:17:14 +0000

    Added some graphs for testing.

---
+++ tests/util.py
@@ -0,0 +1,11 @@
+import os.path
+from rdflib import ConjunctiveGraph
+
+def resource(filename):
+    return os.path.join(os.path.dirname(__file__), 'resources', filename)
+
+def graph(*filenames):
+    graph = ConjunctiveGraph()
+    for filename in filenames:
+        graph.load(resource(filename))
+    return graph
