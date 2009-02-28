f78f0e18a73796c0f0e36b24769da5f6ba81f05d
Brian Beck <exogen@gmail.com>
2009-02-28 16:44:39 -0500

    Added OWL to helpers.__all__.

--- lib/telescope/sparql/helpers.py
+++ lib/telescope/sparql/helpers.py
@@ -7,8 +7,8 @@ from telescope.sparql.expressions import VariableExpressionConstructor, and_, or
 from telescope.sparql.operators import Operator, BuiltinOperatorConstructor
 from telescope.sparql.patterns import union, optional, TriplesSameSubject
 
-__all__ = ['RDF', 'RDFS', 'XSD', 'FN', 'is_a', 'v', 'op', 'fn', 'asc', 'desc',
-           'and_', 'or_', 'union', 'optional']
+__all__ = ['RDF', 'RDFS', 'OWL', 'XSD', 'FN', 'is_a', 'v', 'op', 'fn', 'asc',
+           'desc', 'and_', 'or_', 'union', 'optional']
 
 RDF = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
 RDFS = Namespace('http://www.w3.org/2000/01/rdf-schema#')
