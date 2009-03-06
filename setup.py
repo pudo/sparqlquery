eb74e02af6a0fc5da56cf59fc723a9d1ff163b87
Brian Beck <exogen@gmail.com>
2009-03-06 07:44:20 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-from ez_setup import use_setuptools
-use_setuptools()
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
 
 setup(
     name='telescope',
     version='0.1',
-    description="Observe SPARQLing RDF constellations through Python objects.",
+    keywords='rdf rdflib sparql orm',
+    license='MIT',
+    author="Brian Beck",
+    description="RDF data mapper and SPARQL query builder",
     url='http://code.google.com/p/telescope/',
     packages=find_packages('lib'),
     package_dir={'': 'lib'},
     install_requires=['rdflib<3a'],
-    test_suite='nose.collector'
+    test_suite='nose.collector',
+    tests_require=['nose']
 )
